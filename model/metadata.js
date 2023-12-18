f3b647ae068a15fd31217a711341fee7bde089fa
Thomas Theiner <thomas.theiner@gmx.de>
2023-12-18 23:38:13 +0100

    Avoid skipping videos that have been added later

--- model/metadata.js
+++ model/metadata.js
@@ -1,10 +1,11 @@
 import mongoose from "mongoose";
 
 const metadataSchema = new mongoose.Schema(
-  {
-    lastchecked: { type: Number, required: true, unique: true },
-  },
-  { collection: "metadata" }
+   {
+      lastchecked: { type: Number, required: true, unique: true },
+      lasttwenty: { type: [String] },
+   },
+   { collection: "metadata" }
 );
 
 export const Metadata = mongoose.model("Metadata", metadataSchema);
