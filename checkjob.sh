85c06de790bd4ecc49a6f5f9abc8de15d48557d7
Thomas Theiner <thomas.theiner@gmx.de>
2024-10-01 12:18:11 +0200

    checkjob added

---
+++ checkjob.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+ps -ef | grep [i]ndex >/dev/null
+if [ $? -ne 0 ]; then
+   cd /home/devnull69/Projects/ytsubscriber_mastodon_bot
+   node ./index.js 1>./output.log 2>./error.log &
+fi
+
