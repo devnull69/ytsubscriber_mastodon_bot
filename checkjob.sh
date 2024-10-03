0cb8e58465ce8435977d66da4d829e851de58b64
Thomas Theiner <thomas.theiner@gmx.de>
2024-10-03 09:32:06 +0200

    Bugfix: Now appending to log files again

--- checkjob.sh
+++ checkjob.sh
@@ -2,6 +2,6 @@
 ps -ef | grep [i]ndex >/dev/null
 if [ $? -ne 0 ]; then
    cd /home/devnull69/Projects/ytsubscriber_mastodon_bot
-   node ./index.js 1>./output.log 2>./error.log &
+   node ./index.js 1>>./output.log 2>>./error.log &
 fi
 
